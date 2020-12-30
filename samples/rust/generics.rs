20ab217606c3a930ce96b8db0fe2e447ca8ded92
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-12-30 23:53:58 +0800

    rust: add paradigms

---
+++ samples/rust/generics.rs
@@ -0,0 +1,11 @@
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
